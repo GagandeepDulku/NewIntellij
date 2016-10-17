0ee587e01f12f2f0c3423a6e44ae0e8480016a2b
Gagandeepdulku <gagandeepduklu@gmail.com>
2016-10-17 23:50:40 +0530

    Initial commit

---
+++ Dexter/src/Dexter.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Gagan on 10/16/2016.
+ */
+public class Dexter {
+
+    public static void main(String[] args) {
+        new SampleA();
+    }
+}
---
+++ Dexter/src/SampleA.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Gagan on 10/16/2016.
+ */
+public class SampleA {
+    static {
+        System.out.println("static Block  ");
+    }
+
+    {
+        System.out.println("Initializer");
+    }
+
+}
